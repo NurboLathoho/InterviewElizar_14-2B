a5c08203fda02ad24ace3282ace79687132f97f1
elizar777 <ahmadbekovelizar390@gmail.com>
2024-05-15 18:50:26 +0600

    finished practice

---
+++ main.py
@@ -0,0 +1,53 @@
+"""15)Подсчет количества слов в файле:
+   Создайте программу, которая подсчитывает количество слов в текстовом файле и выводит это число.
+16)Запись в файл:   Попросите пользователя ввести некоторый текст и сохраните его в текстовом файле.
+17)Копирование файла:
+   Напишите программу, которая копирует содержимое одного файла в другой файл."""
+   
+   
+# Задача 1
+
+# def count_words(file_name):
+#     with open(file_name, 'r') as file:
+#             text = file.read()
+#             word_count = len(text.split())
+#             return word_count
+        
+# file_name = "user2.txt"
+# word_count = count_words(file_name)
+# if word_count is not None:
+#     print("Количество слов в файле:", word_count)
+
+
+
+
+
+# Задача 2
+# req = input("Введите текст: ")
+
+# with open("user1.txt", "w") as file_1:
+#     file_1.write(req)
+    
+    
+
+
+# Задача 3
+
+# text = "Loading configuration.... Done loading configuration"
+
+# with open("user2.txt", "w") as file_2:
+#     file_2.write(text)
+    
+# with open("user2.txt", "r") as file_3:
+#     copnig = file_3.read()
+#     print(copnig)
+    
+# with open("user4.txt", "w") as file_4:
+#     file_4.write(copnig)
+    
+
+  
+  
+  
+  
+  
